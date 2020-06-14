a00a90cb2d8564d85a4bc7306f8d478f237e807c
sjavandooren-nv <66804119+sjavandooren-nv@users.noreply.github.com>
2020-06-14 19:18:44 +0200

    Revert "oops"
    This reverts commit c8783b2957abe506e6ed618d1652125d7a28efdf.

---
+++ Views/Hider/HiderInloopkast.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace HideAndSeek.Views
+{
+    public partial class HiderInloopkast : Form
+    {
+        public HiderInloopkast()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_WoonNorth_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            HiderWoonNorth form = new HiderWoonNorth();
+            form.Closed += (s, args) => this.Close();
+            form.Show();
+
+            HttpWebRequest request = WebRequest.Create("http://127.0.0.1:8080/json.htm?type=command&param=switchlight&idx=9&switchcmd=On") as HttpWebRequest;
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MyGlobals.timeInloopkast++;
+            timerLabel.Text = MyGlobals.timeInloopkast.ToString();
+        }
+
+        private void btnEndTurn_Click(object sender, EventArgs e)
+        {
+            MyGlobals.endLocationHider = 1;
+            this.Hide();
+
+            Hider.HiderEndForm form = new Hider.HiderEndForm();
+            form.Closed += (s, args) => this.Close();
+            form.Show();
+        }
+    }
+}
---
+++ Views/Hider/HiderWoonNorth.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace HideAndSeek.Views
+{
+    partial class HiderWoonNorth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnEndTurn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnWest = new System.Windows.Forms.Button();
+            this.btn_South = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEndTurn
+            // 
+            this.btnEndTurn.Location = new System.Drawing.Point(356, 379);
+            this.btnEndTurn.Name = "btnEndTurn";
+            this.btnEndTurn.Size = new System.Drawing.Size(103, 50);
+            this.btnEndTurn.TabIndex = 14;
+            this.btnEndTurn.Text = "End turn";
+            this.btnEndTurn.UseVisualStyleBackColor = true;
+            this.btnEndTurn.Click += new System.EventHandler(this.btnEndTurn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(353, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Location: Woonruimte North";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(125, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(551, 230);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnWest
+            // 
+            this.btnWest.Location = new System.Drawing.Point(220, 274);
+            this.btnWest.Name = "btnWest";
+            this.btnWest.Size = new System.Drawing.Size(133, 68);
+            this.btnWest.TabIndex = 11;
+            this.btnWest.Text = "Go West";
+            this.btnWest.UseVisualStyleBackColor = true;
+            this.btnWest.Click += new System.EventHandler(this.btnUhal_Click);
+            // 
+            // btn_South
+            // 
+            this.btn_South.Location = new System.Drawing.Point(463, 274);
+            this.btn_South.Name = "btn_South";
+            this.btn_South.Size = new System.Drawing.Size(133, 68);
+            this.btn_South.TabIndex = 10;
+            this.btn_South.Text = "Go South";
+            this.btn_South.UseVisualStyleBackColor = true;
+            this.btn_South.Click += new System.EventHandler(this.btn_WoonNorth_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(614, 379);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Time spent:";
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Location = new System.Drawing.Point(682, 379);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(0, 13);
+            this.timerLabel.TabIndex = 15;
+            this.timerLabel.Click += new System.EventHandler(this.timerLabel_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // HiderWoonNorth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.btnEndTurn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnWest);
+            this.Controls.Add(this.btn_South);
+            this.Name = "HiderWoonNorth";
+            this.Text = "HiderWoonNorth";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEndTurn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnWest;
+        private System.Windows.Forms.Button btn_South;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
